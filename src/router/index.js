a684dfe2859de7fe035a28aec31323959495360d
Inanta Martsanto <inanta@gmail.com>
2024-07-30 12:28:59 +0700

    Change router

--- src/router/index.js
+++ src/router/index.js
@@ -5,62 +5,63 @@ const routes = [
   {
     path: "/",
     name: "App Builder View",
-    component: () => import("../views/AppBuilderView.vue")
+    // component: () => import("../views/AppBuilderView.vue")
+    component: () => import("../views/AppBuilder.vue")
   },
-  {
-    path: "/app/builder/view",
-    name: "App Builder View",
-    component: () => import("../views/AppBuilderView.vue")
-  },
+  // {
+  //   path: "/app/builder/view",
+  //   name: "App Builder View",
+  //   component: () => import("../views/AppBuilderView.vue")
+  // },
   {
     path: "/app/builder/build/:name?",
     name: "App Builder",
     component: () => import("../views/AppBuilder.vue")
-  },
-  {
-    path: "/app/view/:name",
-    name: "App View",
-    component: () => import("../views/AppView.vue")
-  },
-  {
-    path: "/app/insert/:name/:parameter?/:value?",
-    name: "App Insert",
-    component: () => import("../views/AppInsert.vue")
-  },
-  {
-    path: "/app/edit/:name/:id",
-    name: "App Edit",
-    component: () => import("../views/AppEdit.vue")
-  },
-
-  ///
-  {
-    path: "/api/builder/view",
-    name: "API Builder View",
-    component: () => import("../views/ApiBuilderView.vue")
-  },
-  {
-    path: "/api/builder/build/:endpoint(.*)?",
-    name: "API Builder",
-    component: () => import("../views/ApiBuilder.vue")
-  },
-  ///
-
-  {
-    path: "/report/builder/build/:name?",
-    name: "Report Builder",
-    component: () => import("../views/ReportBuilder.vue")
-  },
-  {
-    path: "/report/builder/view",
-    name: "Report Builder View",
-    component: () => import("../views/ReportBuilderView.vue")
-  },
-  {
-    path: "/report/view/:name",
-    name: "ReportView",
-    component: () => import("../views/ReportView.vue")
   }
+  // {
+  //   path: "/app/view/:name",
+  //   name: "App View",
+  //   component: () => import("../views/AppView.vue")
+  // },
+  // {
+  //   path: "/app/insert/:name/:parameter?/:value?",
+  //   name: "App Insert",
+  //   component: () => import("../views/AppInsert.vue")
+  // },
+  // {
+  //   path: "/app/edit/:name/:id",
+  //   name: "App Edit",
+  //   component: () => import("../views/AppEdit.vue")
+  // }
+
+  ///
+  // {
+  //   path: "/api/builder/view",
+  //   name: "API Builder View",
+  //   component: () => import("../views/ApiBuilderView.vue")
+  // },
+  // {
+  //   path: "/api/builder/build/:endpoint(.*)?",
+  //   name: "API Builder",
+  //   component: () => import("../views/ApiBuilder.vue")
+  // },
+  // ///
+
+  // {
+  //   path: "/report/builder/build/:name?",
+  //   name: "Report Builder",
+  //   component: () => import("../views/ReportBuilder.vue")
+  // },
+  // {
+  //   path: "/report/builder/view",
+  //   name: "Report Builder View",
+  //   component: () => import("../views/ReportBuilderView.vue")
+  // },
+  // {
+  //   path: "/report/view/:name",
+  //   name: "ReportView",
+  //   component: () => import("../views/ReportView.vue")
+  // }
 ];
 
 const router = createRouter({
