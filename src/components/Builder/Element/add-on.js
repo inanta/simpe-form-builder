14ba4522ed738ca9902b946537a2e7a8df76a708
Inanta Martsanto <inanta@gmail.com>
2024-07-30 12:25:35 +0700

    Change element list

--- src/components/Builder/Element/add-on.js
+++ src/components/Builder/Element/add-on.js
@@ -1,11 +1,1 @@
-import Timeline from "@/components/Builder/Element/AddOn/Timeline.vue";
-import SignaturePad from "@/components/Builder/Element/AddOn/SignaturePad.vue";
-import ActivityFeeds from "@/components/Builder/Element/AddOn/ActivityFeeds.vue";
-import ActivityIndicators from "@/components/Builder/Element/AddOn/ActivityIndicators.vue";
-
-export default {
-  Timeline,
-  SignaturePad,
-  ActivityFeeds,
-  ActivityIndicators
-};
+export default {};
