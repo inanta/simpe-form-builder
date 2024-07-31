9205ff906e57a2fc7970f9b7d4fe13c5316884d8
Inanta Martsanto <inanta@gmail.com>
2024-07-31 14:57:13 +0700

    Modularize and append class if already exists

--- src/assets/js/builder/cleanAttributes.js
+++ src/assets/js/builder/cleanAttributes.js
@@ -1,19 +1,29 @@
-export default function cleanAttributes(attributes) {
+export default function cleanAttributes(
+  attributes,
+  additional_attributes = []
+) {
   let cleaned_attributes = {};
   let allowed_attributes = {
     attrs: false,
+    class: false,
     container: true,
     column: true,
-    disabled: false,
     element: true,
-    multiple: false,
     name: false,
-    placeholder: false,
-    readonly: false,
-    row: true,
-    rows: false,
-    size: false
+    row: true
   };
+
+  for (let index = 0; index < additional_attributes.length; index++) {
+    allowed_attributes[additional_attributes[index]] = false;
+  }
+
+  function setAttribute(name, value) {
+    if (typeof cleaned_attributes[name] !== "undefined" && name === "class") {
+      cleaned_attributes[name] += " " + value;
+    } else {
+      cleaned_attributes[name] = value;
+    }
+  }
 
   for (const key in attributes) {
     if (typeof attributes[key] !== "object" && attributes[key] !== "") {
@@ -21,15 +31,16 @@ export default function cleanAttributes(attributes) {
         if (allowed_attributes[key]) {
           cleaned_attributes["data-" + key] = attributes[key];
         } else {
-          cleaned_attributes[key] = attributes[key];
+          setAttribute(key, attributes[key]);
         }
       }
     } else if (typeof attributes[key] === "object" && key === "attrs") {
       if (Array.isArray(attributes[key].value)) {
         for (let index = 0; index < attributes[key].value.length; index++) {
-          const attribute = attributes[key].value[index];
-
-          cleaned_attributes[attribute.label] = attribute.value;
+          setAttribute(
+            attributes[key].value[index].label,
+            attributes[key].value[index].value
+          );
         }
       }
     }
