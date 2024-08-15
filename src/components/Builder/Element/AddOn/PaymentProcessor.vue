6146eed0715cebd5f3939054acdd518c8dc7f018
Inanta Martsanto <inanta@gmail.com>
2024-08-15 11:58:28 +0700

    Add payment processor element

---
+++ src/components/Builder/Element/AddOn/PaymentProcessor.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <template v-if="builder">
+      <div
+        class="rounded border border-mid-gray p-3 text-center text-lg font-semibold text-mid-gray shadow-inner"
+      >
+        Payment section will be displayed here
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+// import configurations from "@/assets/js/builder/variables/configurations.js";
+import fieldProperties from "@/assets/js/builder/variables/fieldProperties.js";
+// import fieldPropertyOptions from "@/assets/js/builder/variables/fieldPropertyOptions";
+import elementPanelList from "@/assets/js/builder/variables/elementPanelList.js";
+
+export default {
+  inheritAttrs: false,
+  props: {
+    builder: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data: function () {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  methods: {}
+};
+
+elementPanelList.addElement(
+  "general",
+  "Payment Processor",
+  "Payment Processor",
+  "payment-processor",
+  "mdi mdi-cart-check"
+);
+
+fieldProperties["payment-processor"] = {
+  label: {
+    label: "Label"
+  }
+};
+</script>
--- src/components/Builder/Element/add-on.js
+++ src/components/Builder/Element/add-on.js
@@ -1,5 +1,7 @@
 import PaymentOption from "@/components/Builder/Element/AddOn/PaymentOption.vue";
+import PaymentProcessor from "@/components/Builder/Element/AddOn/PaymentProcessor.vue";
 
 export default {
-  PaymentOption
+  PaymentOption,
+  PaymentProcessor
 };
