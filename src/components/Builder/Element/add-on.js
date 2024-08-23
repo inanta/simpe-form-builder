5aa7ab97d92ab0280150d5edfa2298314ee6b2ed
Inanta Martsanto <inanta@gmail.com>
2024-08-23 08:33:09 +0700

    Add RTE

--- src/components/Builder/Element/add-on.js
+++ src/components/Builder/Element/add-on.js
@@ -1,7 +1,9 @@
 import PaymentOption from "@/components/Builder/Element/AddOn/PaymentOption.vue";
 import PaymentProcessor from "@/components/Builder/Element/AddOn/PaymentProcessor.vue";
+import RichTextEditor from "@/components/Builder/Element/RichTextEditor.vue";
 
 export default {
   PaymentOption,
-  PaymentProcessor
+  PaymentProcessor,
+  RichTextEditor
 };
