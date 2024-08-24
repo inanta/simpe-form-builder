66e5d47db2379cc62720f856c727328c950673da
Inanta Martsanto <inanta@gmail.com>
2024-08-24 10:54:51 +0700

    Check if container undefined

--- src/assets/js/builder/onAppWindowScroll.js
+++ src/assets/js/builder/onAppWindowScroll.js
@@ -1,19 +1,24 @@
 export default function (self) {
-  if (window.scrollY > self.$refs.mainContainer.offsetTop) {
-    self.$refs.topButtonsContainer.classList.add("dark:bg-surface--dark-600");
-    self.$refs.topButtonsContainer.classList.add("rounded-t-none");
-    self.$refs.topButtonsContainer.classList.add("shadow");
-    self.$refs.topButtonsContainer.classList.add("sticky");
-    self.$refs.topButtonsContainer.classList.add("top-0");
-    self.$refs.topButtonsContainer.classList.add("z-10");
-  } else {
-    self.$refs.topButtonsContainer.classList.remove(
-      "dark:bg-surface--dark-600"
-    );
-    self.$refs.topButtonsContainer.classList.remove("rounded-t-none");
-    self.$refs.topButtonsContainer.classList.remove("shadow");
-    self.$refs.topButtonsContainer.classList.remove("sticky");
-    self.$refs.topButtonsContainer.classList.remove("top-0");
-    self.$refs.topButtonsContainer.classList.remove("z-10");
+  if (
+    typeof self.$refs.mainContainer != "undefined" &&
+    typeof self.$refs.topButtonsContainer != "undefined"
+  ) {
+    if (window.scrollY > self.$refs.mainContainer.offsetTop) {
+      self.$refs.topButtonsContainer.classList.add("dark:bg-surface--dark-600");
+      self.$refs.topButtonsContainer.classList.add("rounded-t-none");
+      self.$refs.topButtonsContainer.classList.add("shadow");
+      self.$refs.topButtonsContainer.classList.add("sticky");
+      self.$refs.topButtonsContainer.classList.add("top-0");
+      self.$refs.topButtonsContainer.classList.add("z-10");
+    } else {
+      self.$refs.topButtonsContainer.classList.remove(
+        "dark:bg-surface--dark-600"
+      );
+      self.$refs.topButtonsContainer.classList.remove("rounded-t-none");
+      self.$refs.topButtonsContainer.classList.remove("shadow");
+      self.$refs.topButtonsContainer.classList.remove("sticky");
+      self.$refs.topButtonsContainer.classList.remove("top-0");
+      self.$refs.topButtonsContainer.classList.remove("z-10");
+    }
   }
 }
