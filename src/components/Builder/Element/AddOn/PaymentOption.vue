db5e089843199100029b71e8cc0977bef2f57bde
Inanta Martsanto <inanta@gmail.com>
2024-11-26 10:10:58 +0700

    Add remining items and sold out

--- src/components/Builder/Element/AddOn/PaymentOption.vue
+++ src/components/Builder/Element/AddOn/PaymentOption.vue
@@ -1,72 +1,89 @@
 <template>
-  <div class="flex flex-col md:flex-row">
-    <template v-if="paymentOptionType == 'title-description'">
-      <div class="md:pb:0 w-full pb-2 font-bold md:w-3/12">
-        <pre
-          class="whitespace-pre-wrap"
-        ><span class="mdi mdi-circle mr-2 md:hidden"></span>{{ title }}</pre>
-      </div>
-      <div class="md:pb:0 w-full pb-2 md:w-4/12">
-        <pre class="whitespace-pre-wrap">{{ description }}</pre>
-      </div>
-    </template>
-    <template v-else>
-      <div class="md:pb:0 w-full pb-2 font-bold md:w-7/12">
-        <img
-          v-if="paymentOptionPicture !== ''"
-          :src="paymentOptionPicture"
-          class="pr-5"
-        />
-        <div v-else class="text-red-600">Please Add The Picture URL</div>
-      </div>
-    </template>
-    <div class="md:pb:0 ml-auto w-full pb-2 md:w-5/12">
-      <div
-        v-for="(item, itemIndex) in paymentOptionItems"
-        :key="item.name"
-        class="mb-1"
-      >
-        <div class="flex">
-          <div class="flex-1 py-2 font-bold">
-            {{ item.name }}
-          </div>
-          <div class="ml-auto flex-1 px-4 py-2 text-right">
-            {{ formatPrice(item.price) }}
-          </div>
-          <div class="flex-1">
-            <div class="w-max rounded !border !border-mid-gray">
-              <button
-                v-if="!readonly"
-                class="!w-9 !border-r px-3 py-1.5 text-base text-black outline-none"
-                type="button"
-                @click="onMinusButtonClick(itemIndex)"
-              >
-                -
-              </button>
-              <input
-                v-model="quantity[itemIndex]"
-                class="!w-14 appearance-none rounded-sm !border-none bg-white px-3 py-1.5 text-center text-base text-black outline-none focus:border-primary"
-                type="text"
-                min="0"
-                :readonly="readonly"
-              />
-              <button
-                v-if="!readonly"
-                class="!w-9 !border-l px-3 py-1.5 text-base text-black outline-none"
-                type="button"
-                @click="onPlusButtonClick(itemIndex)"
-              >
-                +
-              </button>
+  <div>
+    <div class="flex flex-col md:flex-row">
+      <template v-if="paymentOptionType == 'title-description'">
+        <div class="md:pb:0 w-full pb-2 font-bold md:w-3/12">
+          <pre
+            class="whitespace-pre-wrap"
+          ><span class="mdi mdi-circle mr-2 md:hidden"></span>{{ title }}</pre>
+        </div>
+        <div class="md:pb:0 w-full pb-2 md:w-4/12">
+          <pre class="whitespace-pre-wrap">{{ description }}</pre>
+        </div>
+      </template>
+      <template v-else>
+        <div class="md:pb:0 w-full pb-2 font-bold md:w-7/12">
+          <img
+            v-if="paymentOptionPicture !== ''"
+            :src="paymentOptionPicture"
+            class="pr-5"
+          />
+          <div v-else class="text-red-600">Please Add The Picture URL</div>
+        </div>
+      </template>
+      <div class="md:pb:0 ml-auto w-full pb-2 md:w-5/12">
+        <div
+          v-for="(item, itemIndex) in paymentOptionItems"
+          :key="item.name"
+          class="mb-1"
+        >
+          <div class="flex">
+            <div class="flex-1 py-2 font-bold">
+              {{ item.name }}
             </div>
-          </div>
-          <div class="ml-auto flex-1 px-4 py-2 text-right">
-            {{ formatPrice(quantity[itemIndex] * item.price) }}
+            <div class="ml-auto flex-1 px-4 py-2 text-right">
+              {{ formatPrice(item.price) }}
+            </div>
+            <div class="flex-1">
+              <div class="w-max rounded !border !border-mid-gray">
+                <button
+                  v-if="!readonly"
+                  class="!w-9 !border-r px-3 py-1.5 text-base text-black outline-none"
+                  type="button"
+                  :class="{ 'text-disabled': isSoldOut }"
+                  :disabled="isSoldOut"
+                  @click="onMinusButtonClick(itemIndex)"
+                >
+                  -
+                </button>
+                <input
+                  v-model="quantity[itemIndex]"
+                  class="!w-14 appearance-none rounded-sm !border-none bg-white px-3 py-1.5 text-center text-base text-black outline-none focus:border-primary"
+                  type="text"
+                  min="0"
+                  :class="{ 'text-disabled': isSoldOut }"
+                  :disabled="isSoldOut"
+                  :readonly="readonly"
+                />
+                <button
+                  v-if="!readonly"
+                  class="!w-9 !border-l px-3 py-1.5 text-base text-black outline-none"
+                  type="button"
+                  :class="{ 'text-disabled': isSoldOut }"
+                  :disabled="isSoldOut"
+                  @click="onPlusButtonClick(itemIndex)"
+                >
+                  +
+                </button>
+              </div>
+            </div>
+            <div class="ml-auto flex-1 px-4 py-2 text-right">
+              {{ formatPrice(quantity[itemIndex] * item.price) }}
+            </div>
           </div>
         </div>
       </div>
+      <div></div>
     </div>
-    <div></div>
+    <div v-if="isSoldOut" class="text-center font-bold text-negative">
+      Sorry, this item is no longer available.
+    </div>
+    <div
+      v-else-if="maxQuantityFromServer <= 10"
+      class="text-center font-bold text-negative"
+    >
+      Only {{ maxQuantityFromServer }} more left. Limited availability.
+    </div>
   </div>
 </template>
 
@@ -75,6 +92,7 @@ import configurations from "@/assets/js/builder/variables/configurations.js";
 import fieldProperties from "@/assets/js/builder/variables/fieldProperties.js";
 import fieldPropertyOptions from "@/assets/js/builder/variables/fieldPropertyOptions";
 import elementPanelList from "@/assets/js/builder/variables/elementPanelList.js";
+import postAPI from "@/assets/js/postAPI.js";
 
 export default {
   inheritAttrs: false,
@@ -148,7 +166,10 @@ export default {
   emits: ["input"],
   data: function () {
     return {
+      isSoldOut: false,
       lastWatchChangesID: null,
+      lastFecth: 0,
+      maxQuantityFromServer: 9999,
       quantity: []
     };
   },
@@ -168,13 +189,10 @@ export default {
       return this.paymentOptionDescription;
     },
     maxQuantity: function () {
-      let max = 999;
+      let max = 9999;
 
-      if (
-        this.paymentOptionMax !== "" &&
-        Number.isFinite(this.paymentOptionMax)
-      ) {
-        max = Number.parseFloat(this.paymentOptionMax);
+      if (this.maxQuantityFromServer > 0) {
+        max = this.maxQuantityFromServer;
       }
 
       return max;
@@ -252,6 +270,21 @@ export default {
 
         for (let index = 0; index < this.paymentOptionItems.length; index++) {
           const item = this.paymentOptionItems[index];
+          let quantity = values[index];
+
+          if (typeof quantity === "string") {
+            quantity = parseInt(quantity);
+
+            if (isNaN(quantity)) {
+              quantity = 0;
+            }
+
+            if (quantity < 0) {
+              quantity = 0;
+            }
+
+            this.quantity[index] = quantity;
+          }
 
           composed_values.push({
             title: this.title,
@@ -273,7 +306,33 @@ export default {
       immediate: true
     }
   },
+  mounted: function () {
+    const self = this;
+
+    this.fetch()
+      .then(function (response) {
+        self.maxQuantityFromServer = response.max;
+
+        if (response.max === 0) {
+          self.isSoldOut = true;
+        }
+      })
+      .catch(function () {});
+  },
   methods: {
+    fetch: async function () {
+      const url = window.location.href;
+      const slug = url.split("/").filter(Boolean).pop();
+      const currentTime = new Date();
+
+      if (this.lastFecth < currentTime - 5 * 60 * 1000) {
+        const self = this;
+
+        self.lastFecth = currentTime;
+
+        return postAPI("app/api/v1/elements/" + slug + "/" + this.name, {});
+      }
+    },
     formatPrice: function (price) {
       if (isNaN(price)) {
         price = 0;
@@ -303,6 +362,24 @@ export default {
       }
     },
     onPlusButtonClick: function (index) {
+      const self = this;
+
+      this.fetch()
+        .then(function (response) {
+          if (typeof response.max != "undefined") {
+            self.maxQuantityFromServer = response.max;
+
+            if (response.max === 0) {
+              self.isSoldOut = true;
+            }
+
+            if (self.quantity[index] > self.maxQuantityFromServer) {
+              self.quantity[index] = self.maxQuantityFromServer;
+            }
+          }
+        })
+        .catch(function () {});
+
       if (this.quantity[index] + 1 > this.maxQuantity) {
         this.quantity[index] = this.maxQuantity;
       } else {
